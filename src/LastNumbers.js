932c781f6fc7f0560be2c426e0f3dcc855b55bf2
lra <lra@r73.info>
2020-10-06 16:34:03 +0200

    publish

--- src/LastNumbers.js
+++ src/LastNumbers.js
@@ -6,7 +6,7 @@ function LastNumbers({data}){
     if (!data) return null
     return (
         <Typography  style={{textAlign:'center', marginTop: '1ex',marginBottom: '1ex'}}>
-            Poslední údaje z: {data.datum} | Pozitivní: {data.pozitivnich} | <span style={{color:'red'}}>Hospitalizovaní: {data.hospitalizovanych}</span>
+            Poslední údaje z: {data.datum} | Pozitivní: {data.pozitivnich||0} | <span style={{color:'red'}}>Hospitalizovaní: {data.hospitalizovanych||0}</span>
         </Typography>
       );
 }
